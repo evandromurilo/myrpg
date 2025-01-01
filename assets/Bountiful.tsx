2367a005458fc9b31331d20ea3a57367b54f56f8
murilo <evandromurilob@hotmail.com>
2025-01-01 08:35:28 -0400

    renderiza mapa tmx (tiled) na MainGameScreen

---
+++ assets/Bountiful.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Bountiful" tilewidth="10" tileheight="10" tilecount="1518" columns="33">
+ <image source="Full.png" width="330" height="460"/>
+</tileset>
--- core/src/main/java/com/evandromurilo/myrpg/screens/MainGameScreen.java
+++ core/src/main/java/com/evandromurilo/myrpg/screens/MainGameScreen.java
@@ -1,17 +1,36 @@
 package com.evandromurilo.myrpg.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainGameScreen implements Screen {
-    private SpriteBatch batch;
-    private Texture image;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
 
     public MainGameScreen() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        map = new TmxMapLoader().load("overworld.tmx");
+
+        // aqui são 1/10 porque o tileset é 10px por 10px
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / 10f);
+        camera = new OrthographicCamera();
+
+        // aqui eu digo que quero 30 x 20, que vai ser convertido para a escala
+        // na prática, ele vai esticar os tiles até caberem 30 na horizontal e 20 na vertical
+        camera.setToOrtho(false, 30, 20);
+
+        // começa no top left, o mapa é 100x100
+        camera.position.x = 15;
+        camera.position.y = 90;
     }
 
     @Override
@@ -21,10 +40,12 @@ public class MainGameScreen implements Screen {
 
     @Override
     public void render(float v) {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
     }
 
     @Override
@@ -49,7 +70,7 @@ public class MainGameScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        map.dispose();
+        renderer.dispose();
     }
 }
