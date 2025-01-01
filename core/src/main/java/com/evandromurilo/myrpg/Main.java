1f2eee5bf109f26b596302592ea42271cc7299ec
murilo <evandromurilob@hotmail.com>
2025-01-01 07:40:57 -0400

    setup Game with MainGameScreen

---
+++ core/src/main/java/com/evandromurilo/myrpg/Main.java
@@ -0,0 +1,25 @@
+package com.evandromurilo.myrpg;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.evandromurilo.myrpg.screens.MainGameScreen;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+    private MainGameScreen mainGameScreen;
+
+    @Override
+    public void create() {
+        mainGameScreen = new MainGameScreen();
+        setScreen(mainGameScreen);
+    }
+    @Override
+    public void dispose() {
+        mainGameScreen.dispose();
+    }
+}
---
+++ core/src/main/java/com/evandromurilo/myrpg/screens/MainGameScreen.java
@@ -0,0 +1,55 @@
+package com.evandromurilo.myrpg.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MainGameScreen implements Screen {
+    private SpriteBatch batch;
+    private Texture image;
+
+    public MainGameScreen() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
