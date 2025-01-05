185947594286112967e2d3253a8dbb8935e248fd
murilo <evandromurilob@hotmail.com>
2025-01-05 16:42:12 -0400

    add creatures texture

---
+++ assets/Creatures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Creatures" tilewidth="10" tileheight="10" tilecount="252" columns="14">
+ <image source="Creatures.png" width="140" height="180"/>
+</tileset>
